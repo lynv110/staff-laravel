11d78e06e7737a0e2091cdcf0e5d955edab39c63
=WDS <=lynv110@gmail.com>
2018-05-16 17:05:35 +0700

    Update send mail library

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -1,14 +1,8 @@
 <?php
 
-if (!function_exists('mail_init')){
-    function mail_init($info = []){
-        return app('EmailHandle')->init($info);
-    }
-}
-
 if (!function_exists('mail_send')){
-    function mail_send($info){
-        return app('EmailHandle')->send($info);
+    function mail_send($info, $data, $view){
+        return app('EmailHandle')->send($info, $data, $view);
     }
 }
 
